c868a51b34d108c63e0e6476b412f7f15310af81
Phu Dinh <phudinhtruongk18@gmail.com>
2022-04-12 13:25:23 +0700

    bruh

--- app/healchecker/admin.py
+++ app/healchecker/admin.py
@@ -1,3 +1,25 @@
+from django.urls import path
 from django.contrib import admin
+from django.db import models
+from health_check.views import MainView
+from django.contrib.auth.decorators import user_passes_test
 
-# Register your models here.
+class DummyModel(models.Model):
+    class Meta:
+        verbose_name = 'Heal Check Manually'
+
+def check_admin(user):
+   return user.is_superuser
+
+@user_passes_test(check_admin)
+def dummy_view(request):
+    return MainView.as_view()(request)
+
+@admin.register(DummyModel)
+class DummyModelAdmin(admin.ModelAdmin):
+    def get_urls(self):
+        view_name = '{}_{}_changelist'.format(
+                DummyModel._meta.app_label, DummyModel._meta.model_name)
+        return [
+            path('healcheck/', dummy_view, name=view_name)
+        ]
--- app/healchecker/views.py
+++ app/healchecker/views.py
@@ -1,8 +1,3 @@
 from django.http.response import HttpResponse, JsonResponse
 from .tasks import add
 
-# Create your views here.
-def total(request):
-    res = add.delay(4,5)
-    return HttpResponse(res)
-
